d302e13f61a0b09f9f682fae60f6243b9e697d43
DonThePhan <donny@mapstrat.com>
2023-08-24 11:15:24 -0400

    Move all instrutions to top of index.html

--- 8. External Modules/app.js
+++ 8. External Modules/app.js
@@ -3,7 +3,6 @@ const app = angular.module('modulesApp', [
   /** 2 */ 'ngTagsInput',
 ]);
 
-
 /** 3. We also added a 'tags' variable into the Parent module. This can be used in the sub modules */
 app.controller('TagsDemoCtrl', function () {
   this.tags = [{ text: 'Test1' }, { text: 'Test2' }, { text: 'Test3' }];
